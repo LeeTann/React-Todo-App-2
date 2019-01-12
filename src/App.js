d2f48176816dc2b60f73ab6e89233498740df404
ltann <leeroyleeroy18@gmail.com>
2019-01-11 22:29:00 -0800

    React Todo App 2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import Header from './components/layout/Header'
+import Todos from './components/Todos'
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state ={
+      todos: [
+        {
+          id: 1,
+          task: "take out the trash",
+          completed: false,
+        },
+        {
+          id: 2,
+          task: "code an application",
+          completed: true,
+        },
+        {
+          id: 3,
+          task: "sleep in til 12",
+          completed: false,
+        }
+      ]
+    }
+  }
+
+  // Toggle Complete
+  markComplete = (id) => {
+    console.log(id)
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    }) })
+  }
+
+  // Delete Item
+  deleteItem = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+    //using the spread operater to copy and then filter the todos. 
+    //for each todo item, retrun the todo id that is not equal to the id that was passed in/clicked.
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} deleteItem={this.deleteItem} />
       </div>
     );
   }
